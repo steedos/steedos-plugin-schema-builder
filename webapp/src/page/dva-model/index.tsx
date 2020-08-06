f314f924fa914ea5ae0823f1a34901fba33ecefb
北名 <beiming@beimingdeMacBook-Pro.local>
2020-08-06 22:03:19 +0800

    feat: publish script add

--- webapp/src/page/dva-model/index.tsx
+++ webapp/src/page/dva-model/index.tsx
@@ -37,7 +37,7 @@ export default (({
      modules: [],
      models: [],
      showModel: null,
-     lockMinZoom: true,
+     lockMinZoom: false,
      showInsertModel: null,
      currentModel: null,
      expandedKeys: [],
